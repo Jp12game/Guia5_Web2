f2a988b575f9f49d24fdc00a7b1dd6a0719fd9b9
Jp12game <jos.pol.h@gmail.com>
2026-03-18 13:55:34 -0600

    config

---
+++ practica5/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <h1 className="text-4xl font-bold text-center pt-10 text-blue-600">
+        Task Manager Pro
+      </h1>
+    </div>
+  )
+}
+export default App
