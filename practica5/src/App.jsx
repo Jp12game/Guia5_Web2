401c1dadbe8208a86a76189cd4846a7d1d6e1019
Jp12game <jos.pol.h@gmail.com>
2026-03-18 19:44:25 -0600

    completo

--- practica5/src/App.jsx
+++ practica5/src/App.jsx
@@ -1,10 +1,5 @@
+import AppRouter from './routes/AppRouter';
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <h1 className="text-4xl font-bold text-center pt-10 text-blue-600">
-        Task Manager Pro
-      </h1>
-    </div>
-  )
+  return <AppRouter />;
 }
 export default App
